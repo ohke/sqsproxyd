826a0a620396e45364361aca731bff8a1169c2c0
ohke <kenta1988@gmail.com>
2021-12-14 07:41:36 +0900

    multithreaded workes implementation

--- src/main.rs
+++ src/main.rs
@@ -35,19 +35,27 @@ async fn main() -> Result<()> {
 
     let config = Config::new()?;
 
-    // create sqs client
-    let sqs =
-        Box::new(AwsSqs::new(config.sqs_url.to_string(), config.max_number_of_messages).await);
-    let webhook = Box::new(WebhookImpl::new(config.clone()));
-    let output_sqs: Option<Box<dyn Sqs + Send + Sync>> = match &config.output_sqs_url {
-        None => None,
-        Some(u) => Some(Box::new(
-            AwsSqs::new(u.to_string(), config.max_number_of_messages).await,
-        )),
-    };
+    let mut handles = vec![];
 
-    let daemon = Daemon::new(config, sqs, webhook, output_sqs);
-    let _ = tokio::spawn(async move { daemon.run().await }).await?;
+    for _ in 0..config.worker_concurrency {
+        let config = config.clone();
+        let sqs =
+            Box::new(AwsSqs::new(config.sqs_url.to_string(), config.max_number_of_messages).await);
+        let webhook = Box::new(WebhookImpl::new(config.clone()));
+        let output_sqs: Option<Box<dyn Sqs + Send + Sync>> = match &config.output_sqs_url {
+            None => None,
+            Some(u) => Some(Box::new(
+                AwsSqs::new(u.to_string(), config.max_number_of_messages).await,
+            )),
+        };
+
+        let daemon = Daemon::new(config, sqs, webhook, output_sqs);
+        handles.push(tokio::spawn(async move { daemon.run().await }));
+    }
+
+    for handle in handles {
+        let _ = handle.await.unwrap();
+    }
 
     Ok(())
 }
