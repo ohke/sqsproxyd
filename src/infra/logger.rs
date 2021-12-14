fe3309c361f35b0cd872f43d86ec8cc5e71de1f9
ohke <kenta1988@gmail.com>
2021-12-15 07:44:55 +0900

    logging filter with RUST_LOG

--- src/infra/logger.rs
+++ src/infra/logger.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
 use tracing;
 use tracing_subscriber;
+use tracing_subscriber::EnvFilter;
 
 pub fn setup_logger() -> Result<()> {
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .finish();
+    let filter = EnvFilter::try_from_default_env()?;
+    let subscriber = tracing_subscriber::fmt().with_env_filter(filter).finish();
     tracing::subscriber::set_global_default(subscriber)?;
     Ok(())
 }
