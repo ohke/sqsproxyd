365aee3d3ba6aeaf92ac64c0a74c107aa5e6b70a
ohke <kenta1988@gmail.com>
2021-12-28 10:17:32 +0900

    fix: json serialization from body

--- src/domain/message.rs
+++ src/domain/message.rs
@@ -7,7 +7,7 @@ pub struct Message {
 impl From<aws_sdk_sqs::model::Message> for Message {
     fn from(item: aws_sdk_sqs::model::Message) -> Self {
         Message {
-            body: serde_json::from_str(&item.body.unwrap()).unwrap(),
+            body: item.body.unwrap(),
             receipt_handle: item.receipt_handle.unwrap(),
         }
     }
