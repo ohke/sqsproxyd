6f9da75dc7c142169dc9ae4a4261ece62631082a
ohke <kenta1988@gmail.com>
2022-01-15 09:30:07 +0900

    --aws-endpoint validation

--- src/domain/config.rs
+++ src/domain/config.rs
@@ -5,11 +5,11 @@ use url::Url;
 #[derive(Clone, Debug, PartialEq, StructOpt)]
 #[structopt(name = "sqsproxyd")]
 pub struct Config {
-    #[structopt(long)]
+    #[structopt(long, env = "AWS_ACCESS_KEY_ID")]
     pub aws_access_key_id: Option<String>,
-    #[structopt(long)]
+    #[structopt(long, env = "AWS_SECRET_ACCESS_KEY")]
     pub aws_secret_access_key: Option<String>,
-    #[structopt(long)]
+    #[structopt(long, env = "AWS_SESSION_TOKEN")]
     pub aws_session_token: Option<String>,
     #[structopt(long, env = "SQSPROXYD_AWS_REGION")]
     pub aws_region: Option<String>,
@@ -55,8 +55,14 @@ impl Config {
     pub fn validate(&self) -> Result<()> {
         if !(1 <= self.max_number_of_messages && self.max_number_of_messages <= 10) {
             return Err(anyhow!(
-                "`max_number_of_messages` should be >= 1 and <= 10."
+                "`--max-number-of-messages` should be >= 1 and <= 10."
             ));
+        }
+
+        if self.aws_endpoint.is_some()
+            && (self.aws_access_key_id.is_none() || self.aws_secret_access_key.is_none())
+        {
+            return Err(anyhow!("If `--aws-endpoint` is set, `--aws-access-key-id` and `--aws-secret-access-key` should be set."));
         }
 
         Ok(())
@@ -70,6 +76,9 @@ mod test {
     use std::str::FromStr;
 
     fn set_env_vars() {
+        env::set_var("AWS_ACCESS_KEY_ID", "AWSACCESSKEY");
+        env::set_var("AWS_SECRET_ACCESS_KEY", "AWSSECRETACCESSKEY");
+        env::set_var("AWS_SESSION_TOKEN", "AWSSESSIONTOKEN");
         env::set_var("SQSPROXYD_AWS_REGION", "us-west-1");
         env::set_var("SQSPROXYD_AWS_ENDPOINT", "http://aws-endpoint.env:2222/");
         env::set_var(
@@ -104,9 +113,9 @@ mod test {
         assert_eq!(
             config,
             Config {
-                aws_access_key_id: None,
-                aws_secret_access_key: None,
-                aws_session_token: None,
+                aws_access_key_id: Some("AWSACCESSKEY".to_string()),
+                aws_secret_access_key: Some("AWSSECRETACCESSKEY".to_string()),
+                aws_session_token: Some("AWSSESSIONTOKEN".to_string()),
                 aws_region: Some("us-west-1".to_string()),
                 aws_endpoint: Some("http://aws-endpoint.env:2222/".to_string()),
                 sqs_url: Url::from_str(
